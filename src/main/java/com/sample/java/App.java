d9b72690bb88589e04d82c17ae53745855376869
saiurakrishna <34538779+saiurakrishna@users.noreply.github.com>
2023-01-04 09:31:29 +0530

    updated java code

--- src/main/java/com/sample/java/App.java
+++ src/main/java/com/sample/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Automation World!" );
+        System.out.println( "Hello CI/CD World!" );
     }
 }
