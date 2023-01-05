b00ddbf8be364792857862f9579c1ca9cd5bac1a
saiurakrishna <34538779+saiurakrishna@users.noreply.github.com>
2023-01-05 08:50:10 +0530

    Update App.java

--- src/main/java/com/sample/java/App.java
+++ src/main/java/com/sample/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello CI/CD World!" );
+        System.out.println( "Hello CI/CD automation world!" );
     }
 }
