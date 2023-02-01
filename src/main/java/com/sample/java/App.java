f703d4662f9c241a41dcb50799f8f0b4906d52e0
tellago <122771848+tellago@users.noreply.github.com>
2023-02-01 12:54:15 +0530

    Update App.java

--- src/main/java/com/sample/java/App.java
+++ src/main/java/com/sample/java/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello CI/CD automation world!" );
+        System.out.println( "Hello CI/CD  pipeline automation world!" );
     }
 }
